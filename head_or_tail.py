82445b7bfadea5ea68bdb2c101365dddb2c5d1b2
Mehrnaz <mehrnaz.nemati2000@gmail.com>
2025-01-04 13:21:16 -0500

    retry all projects

---
+++ head_or_tail.py
@@ -0,0 +1,5 @@
+import random
+
+random_number = random.randint(0,1)
+list_of_coin = ["Head", "Tail"]
+print(list_of_coin[random_number])
---
+++ rock_paper-scissor.py
@@ -0,0 +1,56 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+import random
+list_of_items = ["Rock", "Paper", "Scissors"]
+
+play_again = True
+while play_again:
+    user = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. "))
+    user_choice = list_of_items[user]
+    print(user_choice)
+    computer = random.choice(list_of_items)
+    print(computer)
+    if user_choice == "Rock" and computer == "Paper":
+        print("You loose!")
+    elif user_choice == "Rock" and computer == "Scissors":
+        print("You won!")
+    elif user_choice == "Paper" and computer == "Rock":
+        print("You won!")
+    elif user_choice == "Paper" and computer == "Scissors":
+        print("You loose!")
+    elif user_choice == "Scissors" and computer == "Rock":
+        print("You loose!")
+    elif user_choice == "Scissors" and computer == "Paper":
+        print("You Won!")
+    else:
+        print("Tie!")
+    check = input("Do you want to play again? Y or N ")
+    if check == "N":
+        play_again = False
+
+
+
